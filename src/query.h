38838b5b921dd670a37e677d30d5a48412dcd461
Christian Ehringfeld <c.ehringfeld@t-online.de>
2015-08-06 19:49:07 +0200

    license texts

--- src/query.h
+++ src/query.h
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2015 Christian Ehringfeld <c.ehringfeld@t-online.de>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef QUERY_H
 #define QUERY_H
 #include <QString>
@@ -63,7 +78,7 @@ class Query {
     void setSelect(const QList<Expression> &value);
     void setSelect(const QStringList &value);
 
-private:
+  private:
     QList<Expression> select;
     QString selectOption = QStringLiteral("");
     bool distinct = false;
