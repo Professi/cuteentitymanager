8f217691d2b0562147d3f3485c4922b78c3369ef
Christian Ehringfeld <c.ehringfeld@t-online.de>
2015-08-10 00:08:22 +0200

    ...

--- src/entitymanager.cpp
+++ src/entitymanager.cpp
@@ -387,9 +387,7 @@ void EntityManager::savePostPersistedRelations(const QSharedPointer<Entity>
             } else if (r.getType() == RelationType::ONE_TO_ONE
                        && !r.getMappedBy().isEmpty()) {
                 auto e =  EntityInstanceFactory::castQVariant(var);
-                if (this->hasChanged(e)) {
                     this->save(e, true, false);
-                }
                 auto fkProp = EntityHelper::mappedProperty(r, e);
                 if (fkProp.isValid()) {
                     EntityHelper::addEntityToListProperty(e, entity, fkProp);
